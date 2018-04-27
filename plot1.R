b6082406877b59d4b4d2e0427725ff0f50c07c0b
Swami Gopalakrishnan <swaminathan.g@gmail.com>
2018-04-27 22:08:17 +0530

    updated to fix issues

--- plot1.R
+++ plot1.R
@@ -2,16 +2,20 @@ get_base_data <- function() {
     # fetch dataset
     url_to_fetch <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     
+    # setup column classes
+    colClasses=c(rep("character",2), rep("numeric",7))
+    
     # extract and load
     temp <- tempfile()
     download.file(url_to_fetch, temp)
-    data <- read.csv(unz(temp, "household_power_consumption.txt"), sep = ";")
+    data <- read.csv(unz(temp, "household_power_consumption.txt"), sep = ";", na.strings = "?",
+                     colClasses = colClasses, stringsAsFactors = FALSE)
     
     # transform column classes
-    data <- transform(data, Global_active_power = as.numeric(Global_active_power), Date = as.Date(Date))
+    data <- transform(data, Date = as.Date(Date, format="%d/%m/%Y"))
 
     # subset
-    data <- subset(d3, Date == '2007-02-01' | Date =='2007-02-02')
+    data <- subset(data, Date == '2007-02-01' | Date =='2007-02-02')
 }
 
 # load data
